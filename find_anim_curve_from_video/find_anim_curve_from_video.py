e5bd6514f9860bd050175f1c19a5cff1d78a6277
lizj <lizhujin@navinfo.com>
2020-01-03 13:31:27 +0800

    Need not to take abs

--- find_anim_curve_from_video/find_anim_curve_from_video.py
+++ find_anim_curve_from_video/find_anim_curve_from_video.py
@@ -24,7 +24,7 @@ def eval_transform_values(dir, kind, mask=None, m=None):
 			translate = (m[0][2]**2 + m[1][2]**2) ** 0.5
 			values.append(values[-1] + translate)
 		elif kind == 'scale':
-			scale = 0.5 * (abs(m[0][0]) + abs(m[1][1]))
+			scale = 0.5 * (m[0][0] + m[1][1])
 			values.append(values[-1] * scale)
 		elif kind == 'rotate':
 			rotate0 = np.arctan2(-m[0][1], m[0][0])
