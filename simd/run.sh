9fa985076d7025c9782fbde7b147090d8768f8e5
ZhuJin Li <lizhujin@outlook.com>
2020-10-07 23:46:19 +0800

    Enable warnings

--- simd/run.sh
+++ simd/run.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-CC=clang++
+CC="clang++ --std=c++17 -Wall"
 
 echo "===Scalar version with no optimization==="
-$CC --std=c++17 scalar.cpp -o scalar
+$CC scalar.cpp -o scalar
 ./scalar
 
 echo "===Scalar version with O1==="
-$CC --std=c++17 -O1 scalar.cpp -o scalar_O1
+$CC -O1 scalar.cpp -o scalar_O1
 ./scalar_O1
 
 echo "===Scalar version with O3==="
-$CC --std=c++17 -O3 scalar.cpp -o scalar_O3
+$CC -O3 scalar.cpp -o scalar_O3
 ./scalar_O3
 
 echo "===SIMD version with no optimization==="
-$CC --std=c++17 -mavx -mfma simd.cpp -o simd
+$CC -mavx -mfma simd.cpp -o simd
 ./simd
 
 echo "===SIMD version with O3==="
-$CC --std=c++17 -O3 -mavx2 -mfma simd.cpp -o simd_O3
+$CC -O3 -mavx2 -mfma simd.cpp -o simd_O3
 ./simd_O3
 
 echo "===SIMD version with O3 + 4 accumulators==="
-$CC --std=c++17 -O3 -mavx2 -mfma simd_4acc.cpp -o simd_4acc_O3
+$CC -O3 -mavx2 -mfma simd_4acc.cpp -o simd_4acc_O3
 ./simd_4acc_O3
